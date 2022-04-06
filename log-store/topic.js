c32e0e22863bd152e7a4c0df83a48ed65275ec2c
recepergan <rcpergan@gmail.com>
2022-04-07 02:12:06 +0300

    Logstore ile queue yapısı

---
+++ log-store/topic.js
@@ -0,0 +1,36 @@
+const { Kafka }  = require("kafkajs");
+
+createTopic();
+
+async function createTopic(){ // await kullanıyorsan fonk.asenkron olması gerekir
+    try {
+        //Admin Stuff ..
+    const kafka = new Kafka({
+        clientId: "kafka_log_store_client",
+        brokers : ["172.29.192.1:9092"]  //topiclerin bulunduğu yer
+    });
+
+    const admin = kafka.admin();
+    console.log("Kafka Brokera bağlanılıyor..");
+    await admin.connect();
+    console.log("Kafka Brokera bağlantı başarılı,Topic üretielecek");
+    await admin.createTopics({
+        topics : [  //Birden fazla topic üretiyoruz
+            {
+                topic : "LogStoreTopic",
+                numPartitions :2
+            },
+           
+        ]
+    });
+    console.log("Topic başarılı bir şekilde oluşturulmuştur...");
+    await admin.disconnect();
+
+        
+    } catch (error) {
+        console.log("Bir Hata Oluştu", error);
+    }finally{
+        process.exit(0);
+    }
+
+}
